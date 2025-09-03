7edf60c2e094b0a0ec150b1656c9e84c5720bb5e
FT-未雨 <ft_telecom@outlook.com>
2025-09-03 16:08:42 +0800

    Delete src directory

--- src/DisplayConfig.h
+++
@@ -1,7 +0,0 @@
-// DisplayConfig.h -- portable resolution macros for ST7305 Mono TFT
-#ifndef DISPLAY_WIDTH
-  #define DISPLAY_WIDTH   168   // portrait X pixels
-#endif
-#ifndef DISPLAY_HEIGHT
-  #define DISPLAY_HEIGHT  384   // portrait Y pixels
-#endif
